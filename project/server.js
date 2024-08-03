6c3365e8c790de355809365e64b70779cc8f695c
TusharMohapatra07 <tusharmohapatra.gig@gmail.com>
2024-08-03 17:02:18 +0530

    minor tweaks

--- project/server.js
+++ project/server.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const admin = require("./pages/form/firebase"); // Import the initialized admin object
 const ejs = require("ejs");
 const fs = require("fs");
-const path = require('path');
+const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -13,22 +13,20 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
-// app.use(express.static('crowdfunding/'))
-
 // Serve static files (optional, if you have any)
-app.use(express.static("pages/form"));
+app.use(express.static("pages/form/"));
+app.use(express.static(path.join(__dirname, "crowdfunding")));
 
-app.use(express.static(path.join(__dirname, 'mainPage')));
+app.use(express.static(path.join(__dirname, "mainPage")));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, 'mainPage', 'index.html'));
+  res.sendFile(path.join(__dirname, "mainPage", "index.html"));
 });
 
 // Route for serving the form page
 app.get("/form", (req, res) => {
-  res.sendFile(path.join(__dirname, 'pages', 'form', 'form.html'));
+  res.sendFile(path.join(__dirname, "pages", "form", "form.html"));
 });
-
 
 // Route for handling form submission
 app.post("/submit", async (req, res) => {
@@ -88,8 +86,7 @@ app.get("/accepted", async (req, res) => {
   }
 });
 
-
-app.post('/button-click', async (req, res) => {
+app.post("/button-click", async (req, res) => {
   // console.log('Button clicked!');
   const itemId = req.body.id; // Extracting the id sent from the client
   // console.log('Received ID:', itemId);
@@ -101,7 +98,7 @@ app.post('/button-click', async (req, res) => {
 
     // Get the document from the insideOut collection
     const docSnapshot = await insideOutRef.doc(itemId).get();
-    
+
     if (docSnapshot.exists) {
       // Move the document to the approved collection
       await approvedRef.doc(itemId).set(docSnapshot.data());
@@ -109,7 +106,10 @@ app.post('/button-click', async (req, res) => {
       // Optionally, you can delete the original document
       await insideOutRef.doc(itemId).delete();
 
-      res.json({ success: true, message: `Document with ID: ${itemId} has been moved to approved.` });
+      res.json({
+        success: true,
+        message: `Document with ID: ${itemId} has been moved to approved.`,
+      });
     } else {
       res.status(404).json({ success: false, message: "Document not found." });
     }
@@ -118,16 +118,16 @@ app.post('/button-click', async (req, res) => {
     res.status(500).send("Internal Server Error");
   }
 });
-app.post('/detail', async (req, res) => {
+app.post("/detail", async (req, res) => {
   try {
     const { email } = req.body;
-    console.log('Received email:', email);
-    arr=[]
+    console.log("Received email:", email);
+    arr = [];
     // Fetch data from Firestore
     const db = admin.firestore();
     const approvedRef = db.collection("approved");
     const docSnapshot = await approvedRef.doc(email).get();
-    
+
     // Check if document exists
     if (!docSnapshot.exists) {
       throw new Error(`Document with email ${email} not found`);
@@ -135,16 +135,20 @@ app.post('/detail', async (req, res) => {
     arr.push(docSnapshot.data());
     // Render the HTML template with data from docSnapshot
     console.log("Yaha tak toh aa gya");
-    res.render('details', { item: arr });
+    res.render("details", { item: arr });
   } catch (error) {
-    console.error('Error rendering details page:', error);
-    res.status(500).send('Internal Server Error');
+    console.error("Error rendering details page:", error);
+    res.status(500).send("Internal Server Error");
   }
 });
 
-app.get('/project/crowdfunding',(req, res) => {
-  res.sendFile(path.join(__dirname, "crowdfunding", "index.html"));
-})
+app.get("/crowdfunding", (req, res) => {
+  res.sendFile(path.join(__dirname, "crowdfunding", "crowdfunding.html"));
+});
+
+app.get("/community.html", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "community.html"));
+});
 
 // Start the server
 app.listen(PORT, () => {
